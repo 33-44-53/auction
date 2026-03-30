271850aa53d45dd57603481db48cb3cf69567a7f
umer <uselahadin@gmail.com>
2026-03-30 23:49:09 +0300

    fix: add vercel.json to frontend for SPA routing

--- push-fix.bat
+++ push-fix.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "fix: correct migration_lock.toml format"
+"C:\Program Files\Git\bin\git.exe" commit -m "fix: add vercel.json to frontend for SPA routing"
 "C:\Program Files\Git\bin\git.exe" push origin master
 pause
