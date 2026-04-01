e6d64ce4335f01083e0b8ede68457cd3f2242c4c
umer <uselahadin@gmail.com>
2026-04-02 01:06:31 +0300

    feat: add database migration for group-level metadata fields

---
+++ prisma/migrations/20250101000000_add_group_metadata/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Group" ADD COLUMN "title" TEXT,
+ADD COLUMN "date" TEXT,
+ADD COLUMN "location" TEXT,
+ADD COLUMN "responsibleBody" TEXT,
+ADD COLUMN "exchangeRate" DOUBLE PRECISION;
